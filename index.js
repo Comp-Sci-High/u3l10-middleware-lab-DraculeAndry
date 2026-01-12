261c1b65c7ec0d168cb0e4a8c5fb6ab2a3f05164
Andry <andry.hernandez26@compscihigh.org>
2026-01-12 21:56:05 +0000

    I have completed the lab.

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 // INSTRUCTIONS: Write the code that does the following:
 // -0 Set up the server correctly
+const express = require("express")
+const app = express()
+
+app.use(express.static(__dirname + "/public"))
 // -1 console logs the method and URL for every request
+app.use((request, response, next) => {
+    console.log(response.method + " /" + request.url)
+    next()
+})
 // -2 sends back "Welcome to the Animal Shelter Network" as an h1 when the client goes to /
+app.get("/", (request, response) => {
+    response.send("Welcome to the Animal Shelter Network")
+})
 // -3 sends back the cat object when the client goes to /api/cat
+app.get("/api/cat", (request, response) => {
+    response.json(animalShelterData.cats)
+})
 // -4 sends back the shelters object when the client goes to /api/shelters
+app.get("/api/shelters", (request, response) => {
+    response.json(animalShelterData.shelters)
+})
 // -5 sends back "Go to /api/cat to see cats for adoption and /api/shelters to see shelters in the area" when the client goes to /docs
+app.get("/docs", (request, response) => {
+    response.send("Go to /api/cat to see cats for adoption and /api/shelters to see shelters in the area")
+})
 // -6 BLUE & PINK: sends back "A cat you can adopt is " with the name of the first cat when the client goes to /adopt/cat 
 //    (Use dot notation & string concatenation)
 // -7 sends back a 404 page for all other paths
-
+app.use((req, res, next) => {
+    res.status(404).send("404 Not Found")
+})
 // PINK Only: Add the correct status codes to ALL route handlers
 
 const animalShelterData = {
